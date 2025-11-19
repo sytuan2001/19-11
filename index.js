c328bfff21c9b1c5bf1663186e7bd37c55ff3a47
sytuan2001 <thuancat1978@gmail.com>
2025-11-19 10:19:33 +0700

    add

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log('Happy developing ✨')
+let x = 0;
+let array = Array();
+function add_element_to_array() {
+    array[x] = document.getElementById("txtValue").value;
+    alert("Element: " + array[x] + " Added at index " + x);
+    x++;
+    document.getElementById("txtValue ").value = "";
+}
+function display_array() {
+    let e = "<hr/>";
+    for (let i = 0; i < array.length; i++) {
+        e += "Element " + i + " = " + array[i] + "<br/>";
+    }
+    document.getElementById("result").innerHTML = e;
+}
